929b3eec7c16d9a85da71255cc81da36c35b2254
jonaisenberg <jonathan.araos.a@gmail.com>
2023-10-27 22:52:32 -0300

    agregando boton y Link para volver al incio

--- src/view/NotFound.jsx
+++ src/view/NotFound.jsx
@@ -1,11 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom"
 import Error from "../gif/404.gif"
 
 const NotFound = () => {
     return (
         <div className="imagen">
-            <img src={Error} alt="imagen de error 404" />
+            <img className="gif" src={Error} alt="imagen de error 404" />
+            <h3 className="no-pagina">Página no encontrada.</h3>
+            <Link to="/"><Button variant="success">Volver al inicio</Button></Link>
         </div>
     )
 };
